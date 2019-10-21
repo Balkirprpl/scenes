51985f2ec95a7f797886ae09157c123157664c98
J Gabriel <55000002+Balkirprpl@users.noreply.github.com>
2019-10-21 17:14:09 -0300

    Add files via upload

---
+++ gameover.js
@@ -0,0 +1,9 @@
+var gameover = new Phaser.Scene("gameOver");
+
+gameover.preload = function () {}
+gameover.create = function () {}
+gameover.update = function () {}
+
+export {
+    gameover
+};
---
+++ main.js
@@ -0,0 +1,13 @@
+import {
+    gameover
+} from "./gameover.js";
+
+var main = new Phaser.Scene("Main");
+
+main.preload = function () {}
+main.create = function () {}
+main.update = function () {}
+
+export {
+    main
+};
